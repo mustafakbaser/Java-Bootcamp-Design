008552f814d14dc85d7be9a7e1f5cb5278e5dec7
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2023-11-01 19:34:36 +0300

    Initial Commit

---
+++ app/src/main/java/net/mustafabaser/tasarimcalismasi/MainActivity.java
@@ -0,0 +1,15 @@
+package net.mustafabaser.tasarimcalismasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // R res (Resources)'a referans geliyor.
+    }
+}
